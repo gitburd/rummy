512f5c6a6e4b1e08a9122da08aa7d773be6e8fe7
Dena Burd <denaburd@gmail.com>
2021-05-06 16:58:21 -0700

    add count_meld_check() Player method to recognize 3 or more value meld add run_meld_check() Player method to identify 3 in a row of a suit

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ class Player:
         self.Hand = Hand
         self.Score = self.set_score()
         self.cards = self.set_cards()
+        self.meld_cards = np.array([])
+        self.unmatched_cards = self.Hand.copy()
+        self.runs=np.array([])
+        self.sets=np.array([])
  
     def __str__(self):
         currentHand = ""
@@ -26,9 +30,7 @@ class Player:
         }
 
         for card in self.Hand:
-            # print(f"HEY!!!!!!!!! {card['Value']}{switcher[card['Suit']]}")
             card_key = f"{card['Value']}{switcher[card['Suit']]}"
-            # print(self.cards[card_key])
             user_cards[card_key] = True
 
         self.cards = user_cards
@@ -36,13 +38,13 @@ class Player:
 
     def set_score(self):
         score = 0
-        cardValues = {            
+        card_values = {
             "A" : 11, "K" : 10, "Q" : 10, "J" : 10, 
             "10":10, "9":9, "8":8, "7":7, "6":6, 
             "5":5, "4":4, "3":3, "2":2
         }
         for card in self.Hand:
-            score += cardValues[str(card['Value'])]
+            score += card_values[str(card['Value'])]
             # aces can be 1 or 11
         self.Score = score
         return score
@@ -60,18 +62,7 @@ class Player:
     def play(self, new_hand):
         self.Hand = new_hand
         self.score = self.set_score()
-    
 
-    def win(self, result):
-        if result:
-            if self.Score == 21 and len(self.Hand == 2):
-                print("blackjack")
-            else:
-                print('win')
-    
-    def draw(self):
-        print('tie')
-    
     def print_cards(self):
         print(self.cards)
     
@@ -83,7 +74,8 @@ class Player:
             ":diamond_suit:":"D"
         }
 
-        meld_cards = []
+        self.meld_cards = np.array([])
+        self.unmatched_cards = self.Hand.copy()
 
         for card in self.Hand:
             count = 0
@@ -96,12 +88,114 @@ class Player:
             if self.cards[f"{card['Value']}D"] == True:
                 count += 1
 
-            # if count > 2:
-                # meld_cards.append
-
-            print(card['Value'], "------count:", count)
-
+            if count > 2:
+                self.meld_cards = np.append(self.meld_cards, card)
+                self.unmatched_cards = self.unmatched_cards[self.unmatched_cards != card]
         
+        print("um", len(self.unmatched_cards))
+        print("mc", len(self.meld_cards))
+
+    def suit_pair_check(self):
+        switcher={
+            ":heart_suit:":"H",
+            ":spade_suit:":"S",
+            ":club_suit:":"C",
+            ":diamond_suit:":"D"
+        }
+
+        card_values = {            
+            "A" : 1, "K" : 13, "Q" : 12, "J" : 11,
+            "10":10, "9":9, "8":8, "7":7, "6":6,
+            "5":5, "4":4, "3":3, "2":2
+        }
+        valueCards = {11:"J", 12:"Q", 13:"K"}
+
+        for card in self.Hand:
+            try:
+                card_value = card_values[str(card['Value'])]
+                print("!!!!!!card value", card_value)
+                above = int(card_value) + 1
+                below = int(card_value) - 1
+
+                if above > 10:
+                    above = valueCards[above]
+                if below > 10:
+                    below = valueCards[below]
+                if below == 1:
+                    below = "A"
+                if below == 0:
+                    below == None
+                if above == 14:
+                    above = None
+                print("?????", above, below)
+
+                if above and self.cards[f"{above}{switcher[card['Suit']]}"] == True:
+                    print("MATCH ABOVE MF!!!!!!")
+
+                if below and self.cards[f"{below}{switcher[card['Suit']]}"] == True:
+                    print("MATCH BELOW MF!!!!!!")
+
+            except Exception as e:
+                print("GOT THIS, ", e)
+
+
+    def run_meld_check(self):
+        switcher={
+            ":heart_suit:":"H",
+            ":spade_suit:":"S",
+            ":club_suit:":"C",
+            ":diamond_suit:":"D"
+        }
+
+        card_values = {
+            "A" : 1, "K" : 13, "Q" : 12, "J" : 11,
+            "10":10, "9":9, "8":8, "7":7, "6":6,
+            "5":5, "4":4, "3":3, "2":2
+        }
+        valueCards = {11:"J", 12:"Q", 13:"K"}
+
+        for card in self.Hand:
+            try:
+                card_value = card_values.get(str(card['Value']))
+                # print("!!!!!!card value", cardValue)
+                above = int(card_value) + 1
+                below = int(card_value) - 1
+
+                if above == 14:
+                    above = None
+                elif above > 10:
+                    above = valueCards.get(above)
+
+                if below == 0:
+                    below == None
+                elif below == 1:
+                    below = "A"
+                elif below > 10:
+                    below = valueCards.get(below)
+
+                # print("?????", above, below)
+
+                if (above and self.cards[f"{above}{switcher[card['Suit']]}"] == True
+                    and below and self.cards[f"{below}{switcher[card['Suit']]}"] == True):
+                    print("3 CARD RUN!!!!!!")
+                    self.runs = np.append(self.runs, card)
+                    self.unmatched_cards = self.unmatched_cards[self.unmatched_cards != card]
+
+                    for user_card in self.Hand:
+                        if user_card['Suit'] == card['Suit'] and user_card['Value'] == above:
+                            self.runs = np.append(self.runs, user_card)
+                            self.unmatched_cards = self.unmatched_cards[self.unmatched_cards != user_card]
+                        elif user_card['Suit'] == card['Suit'] and user_card['Value'] == below:
+                            self.runs = np.append(self.runs, user_card)
+                            self.unmatched_cards = self.unmatched_cards[self.unmatched_cards != user_card]
+
+                    print('len runs', len(self.runs))
+                    print('unmatched,', len(self.unmatched_cards))
+
+            except Exception as e:
+                print("GOT THIS, ", e)
+
+
 
 
 def create_deck():
@@ -112,7 +206,7 @@ def create_deck():
         club={"Value":card, "Suit": ":club_suit:"}
         dimond={"Value":card, "Suit": ":diamond_suit:"}
         deck = np.append(deck, [heart,spade,club,dimond])
-    
+
     faceValues = ["J", "Q", "K", "A"]
     for card in faceValues: # add face values
         heart={"Value":str(card), "Suit":":heart_suit:"}
@@ -165,6 +259,7 @@ while True:
     # print(Human_Player.print_cards())
     # print(House.print_cards())
     Human_Player.count_meld_check()
+    Human_Player.run_meld_check()
     while True:
         print("---- Second while ----")
         print(f"Discard: {print_card(discard)}")
@@ -200,15 +295,11 @@ while True:
             else:
                 print('Enter a number 1-10 to discard')
         except Exception as e:
-            print('Enter a number 1-10 to discard')        
-            
+            print('Enter a number 1-10 to discard')
+
     print("---- HOuse turn ----")
     House.hit(discard)
     House.discard_card(int(action))
     print("\nHouse: ", House)
     print(f"Discard: {print_card(discard)}")
-               
-        
-
-
 
